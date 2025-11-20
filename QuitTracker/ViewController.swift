e7fee5b2eb727e45286a4b07f7e855a47bfdc586
AlexanderPavelGonzalez <gonzalez.al@northeastern.edu>
2025-11-20 13:34:51 -0500

    Initial Commit

---
+++ QuitTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuitTracker
+//
+//  Created by Alexander Gonzalez on 11/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
