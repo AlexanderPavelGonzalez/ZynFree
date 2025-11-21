7c215e29a66f2590e244b31b12260a98288c4844
AlexanderPavelGonzalez <gonzalez.al@northeastern.edu>
2025-11-20 19:17:34 -0500

    Update icon image

--- ZynFree/Views/QuitIcon.swift
+++ ZynFree/Views/QuitIcon.swift
@@ -34,23 +34,51 @@ struct QuitIcon: View {
                 .foregroundColor(zColor)
             
             // Red diagonal slash (prohibition line from top-left to bottom-right)
+            // Extends to the circle border, accounting for the stroke width
             Path { path in
-                let radius = size * 0.45
                 let center = size / 2
-                let diagonalLength = radius * sqrt(2)
+                // Use actual radius to edge of circle (accounting for stroke being centered)
+                let radius = size / 2 - circleLineWidth / 2
                 
-                // Start point (top-left area of circle)
-                let startX = center - diagonalLength / 2
-                let startY = center - diagonalLength / 2
+                // Calculate diagonal from center to edge
+                let diagonalOffset = radius * sqrt(2) / 2
                 
-                // End point (bottom-right area of circle)
-                let endX = center + diagonalLength / 2
-                let endY = center + diagonalLength / 2
+                // Start point (top-left area, extending slightly beyond border for visual connection)
+                let startX = center - diagonalOffset
+                let startY = center - diagonalOffset
+                
+                // End point (bottom-right area, extending slightly beyond border)
+                let endX = center + diagonalOffset
+                let endY = center + diagonalOffset
                 
                 path.move(to: CGPoint(x: startX, y: startY))
                 path.addLine(to: CGPoint(x: endX, y: endY))
             }
             .stroke(slashColor, style: StrokeStyle(lineWidth: slashLineWidth, lineCap: .round))
+        }
+        .frame(width: size, height: size)
+    }
+}
+
+// MARK: - App Icon Helper
+extension QuitIcon {
+    /// Creates a view suitable for app icon with black background
+    static func appIcon(size: CGFloat = 1024) -> some View {
+        ZStack {
+            // Black background
+            Color.black
+                .frame(width: size, height: size)
+            
+            // Icon centered with appropriate sizing
+            QuitIcon(
+                size: size * 0.8,  // Use 80% of canvas size for padding
+                backgroundColor: AppTheme.Light.primary,
+                zColor: .black,
+                circleColor: .red,
+                slashColor: .red,
+                circleLineWidth: size * 0.004,  // Scale line width proportionally
+                slashLineWidth: size * 0.006    // Scale line width proportionally
+            )
         }
         .frame(width: size, height: size)
     }
@@ -63,6 +91,6 @@ struct QuitIcon: View {
         QuitIcon(size: 150)
     }
     .padding()
-    .background(Color.gray.opacity(0.2))
+    .background(Color.black)
 }
 
